e4890a79bb9aae26da657bb3cce543b3a9c5f161
app-kaisha <app.kaihatsusha@gmail.com>
2026-02-21 21:13:33 +0000

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,18 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by app-kaihatsusha on 21/02/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
