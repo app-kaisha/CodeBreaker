58663cd20a66b0753fe30c30fb57db97676b19e0
app-kaisha <app.kaihatsusha@gmail.com>
2026-03-01 17:11:28 +0000

    Refactor for mac and Xcode Update

---
+++ CodeBreaker/Views/GameList.swift
@@ -0,0 +1,19 @@
+//
+//  GameList.swift
+//  CodeBreaker
+//
+//  Created by app-kaihatsusha on 01/03/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+struct GameList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameList()
+}
